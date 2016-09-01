654525b05c09f954fc2161c7c0bb9f4acde014ef
Patrick Fitzsimmons <pat@pfitz.net>
2016-08-31 22:51:10 -0400

    fixing sql

--- src/main/resources/sql/00030-job-status-new-columns.mysql.sql
+++ src/main/resources/sql/00030-job-status-new-columns.mysql.sql
@@ -3,6 +3,5 @@ ALTER TABLE stallion_job_status
    ADD COLUMN lockGuid varchar(50),
    ADD COLUMN nextExecuteMinuteStamp varchar(12),
    ADD COLUMN nextExecuteAt BIGINT DEFAULT 0,
-   ADD KEY nextExecuteKey (nextExecuteMinuteStamp),
-   ADD COLUMN `row_updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+   ADD KEY nextExecuteKey (nextExecuteMinuteStamp)
    ;
