a49601a93a0bf299c351ada17b7360513ad532a7
Patrick Fitzsimmons <pat@pfitz.net>
2016-09-02 15:40:15 -0400

    fixing null columns

--- src/main/resources/sql/00030-job-status-new-columns.mysql.sql
+++ src/main/resources/sql/00030-job-status-new-columns.mysql.sql
@@ -1,6 +1,6 @@
 ALTER TABLE stallion_job_status
    ADD COLUMN lockedAt BIGINT NOT NULL DEFAULT 0,
-   ADD COLUMN lockGuid varchar(50),
+   ADD COLUMN lockGuid varchar(50) NOT NULL DEFAULT '',
    ADD COLUMN nextExecuteMinuteStamp varchar(12),
    ADD COLUMN nextExecuteAt BIGINT DEFAULT 0,
    ADD KEY nextExecuteKey (nextExecuteMinuteStamp)
