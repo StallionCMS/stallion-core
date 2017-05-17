1b06b96d40f02b3fa55aed60944e44e5e0a19a93
Patrick Fitzsimmons <pat@pfitz.net>
2017-05-17 11:53:20 -0400

    fixing client exception

--- src/main/java/io/stallion/exceptions/ClientException.java
+++ src/main/java/io/stallion/exceptions/ClientException.java
@@ -17,11 +17,16 @@
 
 package io.stallion.exceptions;
 
+import java.util.Map;
+
+import static io.stallion.utils.Literals.map;
+
 /**
  * The end user did something wrong and needs to correct their request.
  * Will short-circuit the request and return a 400 error by default.
  */
 public class ClientException extends WebException {
+    private Map extra = map();
 
     /**
      * Message will get displayed to the end user, so make it polite and friendly.
@@ -39,6 +44,12 @@ public class ClientException extends WebException {
         super(message, statusCode);
     }
 
+
+    public ClientException(String message, int statusCode, Map extra) {
+        super(message);
+        this.setStatusCode(statusCode);
+        this.extra = extra;
+    }
 
 
     /**
