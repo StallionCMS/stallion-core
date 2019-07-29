e005fc0e214ae3f7162419829a16a05fa54ef952
Patrick Fitzsimmons <pat@pfitz.net>
2019-07-29 12:53:43 -0400

    updating slugify

--- src/main/java/io/stallion/utils/GeneralUtils.java
+++ src/main/java/io/stallion/utils/GeneralUtils.java
@@ -65,6 +65,8 @@ public class GeneralUtils {
     private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
     private static final Pattern MULTIHYPHENS = Pattern.compile("\\-\\-+");
     private static final Pattern ENDING_HYPHENS = Pattern.compile("\\-+$");
+    private static final Pattern STARTING_HYPHENS = Pattern.compile("^\\-+");
+
     // Because probeContentType doesn't work on all platforms;
     private static final Map<String, String> mimeTypes = map(
             val("css", "text/css"),
@@ -90,12 +92,13 @@ public class GeneralUtils {
      * @return
      */
     public static String slugify(String input) {
-        input = input.replace("&", " and ");
-        input = input.replace("$", " dollar ");
+        input = input.replace("\\&", " and ");
+        input = input.replace("\\$", " dollar ");
         String nowhitespace = WHITESPACE.matcher(input).replaceAll("-");
         String normalized = Normalizer.normalize(nowhitespace, Normalizer.Form.NFD);
         String slug = MULTIHYPHENS.matcher(NONLATIN.matcher(normalized).replaceAll("-")).replaceAll("-");
         slug = ENDING_HYPHENS.matcher(slug).replaceAll("");
+        slug = STARTING_HYPHENS.matcher(slug).replaceAll("");
         return slug.toLowerCase(Locale.ENGLISH);
     }
 
